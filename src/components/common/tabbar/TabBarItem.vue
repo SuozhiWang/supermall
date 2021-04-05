7efae8bd80f3dd91390827c0647e49812c3b11e3
wsz <1298643812@qq.com>
2021-04-05 20:29:27 +0800

    '增加tabbar'

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,69 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: wsz
+ * @Date: 2021-04-01 12:07:21
+ * @LastEditors: wsz
+ * @LastEditTime: 2021-04-01 14:20:15
+-->
+<template>
+  <!--所有的item都展示同一个图片, 同一个文字-->
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    //父传子 把要跳转的路径传入
+    props: {
+      path: String,   //动态传入图片加载路径
+      activeColor: {  //动态传入激活状态时的颜色，默认为红色
+        type: String,
+        default: 'red'
+      }
+    },
+    data() {
+      return {
+        // isActive: true
+      }
+    },
+    computed: {
+      isActive() {
+        // /home -> item1(/home) = true
+        // /home -> item1(/category) = false
+        // /home -> item1(/cart) = true
+        // /home -> item1(/profile) = true
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+      activeStyle() {
+        return this.isActive ? {color: this.activeColor} : {}
+      }
+    },
+    methods: {
+      itemClick() {
+        this.$router.replace(this.path)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
